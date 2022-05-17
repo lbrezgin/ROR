0f1436d04441707a562f96bb779a1df76a4987eb
lbrezgin <90979712+lbrezgin@users.noreply.github.com>
2022-05-17 23:08:07 +0300

    Create 01.rb

---
+++ lesson1/01.rb
@@ -0,0 +1,11 @@
+puts "Назовите свое имя:"
+	name = gets.chomp
+puts "Назовите свой рост:"
+	growth = gets.chomp
+ideal_weight = (growth.to_i - 110) * 1.15
+
+	if ideal_weight < 0
+		puts "#{name}, Ваш вес уже оптимальный."
+	else
+		puts "#{name}, Ваш идеальный вес: #{ideal_weight}кг."
+	end
