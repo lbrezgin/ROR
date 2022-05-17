71189a62ee202fbc7233dcd20da7812cdd9e78aa
lbrezgin <lewickbrez@gmail.com>
2022-05-18 00:18:16 +0300

    new

--- lesson1/01.rb
+++ lesson1/01.rb
@@ -7,5 +7,5 @@ ideal_weight = (growth.to_i - 110) * 1.15
 	if ideal_weight < 0
 		puts "#{name}, Ваш вес уже оптимальный."
 	else
-		puts "#{name}, Ваш идеальный вес: #{ideal_weight}кг."
+		puts "#{name}, Ваш идеальный вес: #{ideal_weight}кг"
 	end
