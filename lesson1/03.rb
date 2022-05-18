4284f2cbe412a10138cd76013e38b40a3d4ed463
lbrezgin <lewickbrez@gmail.com>
2022-05-18 21:00:05 +0300

    corrected

--- lesson1/03.rb
+++ lesson1/03.rb
@@ -1,12 +1,11 @@
 puts "Введите стороны треугольника:"
 print "a: "
-	a = gets.chomp.to_i
+  a = gets.chomp.to_i
 print "b: "
 	b = gets.chomp.to_i
 print "c: "
 	c = gets.chomp.to_i
-
-
+  
 if (a > b && a > c) && a**2 == b**2 + c**2 
   puts "Этот треугольник - прямоугольный."
 elsif (b > a && b > c) && b**2 == a**2 + c**2 
