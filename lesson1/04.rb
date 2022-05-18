f4f89072754c6df76e2ee158af563c0b6b6b06fb
lbrezgin <90979712+lbrezgin@users.noreply.github.com>
2022-05-18 13:22:57 +0300

    Update 04.rb

--- lesson1/04.rb
+++ lesson1/04.rb
@@ -14,11 +14,9 @@ print "c: "
 		x1 = (-b + Math.sqrt(d)) / (2*a)
 		x2 = (-b - Math.sqrt(d)) / (2*a)
 		puts "D = #{d}, x1 = #{x1}, x2 = #{x2}"
-
 	elsif d == 0
 		x = -b / (2*a).to_f
 		puts "D = #{d}, x = #{x}"
-	elsif
-		d < 0
+	elsif d < 0
 		puts "D = #{d}, корней нет."
 	end
