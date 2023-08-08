5ab667807deae859fb677416ebc2fbda28bc264c
lbrezgin <lewickbrez@gmail.com>
2023-08-08 21:13:48 +0300

    improvemnet right_triangle

--- lesson1/right_triangle.rb
+++ lesson1/right_triangle.rb
@@ -3,21 +3,8 @@ a = gets.chomp.to_i
 b = gets.chomp.to_i
 c = gets.chomp.to_i
 
-def find_longest(a,b,c)
-  longest = a 
-  if longest < b 
-    longest = b 
-    if longest < c  
-      longest = c 
-    end
-  elsif longest < c 
-    longest = c 
-  end
-  longest
-end
-
 array = [a,b,c]
-hipo = array.delete(find_longest(a, b, c))
+hipo = array.sort!.pop
 if hipo**2 == array.inject(0){ |sum, x| sum + x**2 }
   puts "Это прямоугольный треугольник"
 elsif a == b || b == c || a == c
