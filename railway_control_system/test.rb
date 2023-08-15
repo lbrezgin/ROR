e0ee7df00b6b1b2bc4637d1cacce0f9b0f86e5af
lbrezgin <lewickbrez@gmail.com>
2023-08-15 21:31:31 +0300

    change folder name

---
+++ railway_control_system/test.rb
@@ -0,0 +1,35 @@
+require_relative 'lib/instance_counter' 
+require_relative 'lib/manufacturer' 
+require_relative 'lib/station'  
+require_relative 'lib/train'  
+require_relative 'lib/route' 
+require_relative 'lib/wagon'
+require_relative 'lib/cargo_wagon'  
+require_relative 'lib/passenger_wagon'  
+require_relative 'lib/passenger_train'  
+require_relative 'lib/cargo_train'
+
+
+station1 = Station.new('Riga')
+station2= Station.new('Liepaja')
+station3 = Station.new('Jurmala')
+
+passenger_train = PassengerTrain.new("1234")
+
+route = Route.new(station1, station2)
+route.add_station(station3)
+
+passenger_train.assign_route(route)
+
+wagon1 = PassengerWagon.new
+wagon2 = PassengerWagon.new
+
+passenger_train.attach_a_wagon(wagon1)
+passenger_train.attach_a_wagon(wagon2)
+passenger_train.unhook_the_wagon
+
+passenger_train.go_next_station
+passenger_train.go_previous_station
+
+p route.stations
+station1.trains
