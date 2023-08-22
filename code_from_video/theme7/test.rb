66546e321c748c3d163f360faacb331eb8aa6c4a
lbrezgin <lewickbrez@gmail.com>
2023-08-22 14:53:57 +0300

    chnege

--- code_from_video/theme7/test.rb
+++ code_from_video/theme7/test.rb
@@ -7,3 +7,13 @@ class String
     capitalize
   end
 end
+def destroy
+  @object = Object.find(params[:id])
+  if @object.destroy
+    flash[:success] = 'Object was successfully deleted.'
+    redirect_to objects_url
+  else
+    flash[:error] = 'Something went wrong'
+    redirect_to objects_url
+  end
+end
