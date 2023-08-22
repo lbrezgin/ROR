8eeee043d6cbb590d27ba9fd4790f9a0b630ee83
lbrezgin <lewickbrez@gmail.com>
2023-08-22 14:56:01 +0300

    delete

--- code_from_video/theme7/test.rb
+++ code_from_video/theme7/test.rb
@@ -7,13 +7,3 @@ class String
     capitalize
   end
 end
-def destroy
-  @object = Object.find(params[:id])
-  if @object.destroy
-    flash[:success] = 'Object was successfully deleted.'
-    redirect_to objects_url
-  else
-    flash[:error] = 'Something went wrong'
-    redirect_to objects_url
-  end
-end
--- rubocop_pre_commit_hook.rb
+++ rubocop_pre_commit_hook.rb
@@ -1,25 +1,8 @@
-ADDED_OR_MODIFIED = /^\s*(A|AM|M)/.freeze
+#!/bin/bash
 
-changed_files = `git status --porcelain`.split(/\n/)
-unstaged_files = `git ls-files -m`.split(/\n/)
-
-changed_files = changed_files.select { |f| f =~ ADDED_OR_MODIFIED }
-changed_files = changed_files.map { |f| f.split(" ")[1] }
-
-changed_files -= (unstaged_files - changed_files)
-
-changed_files = changed_files.select { |file_name| File.extname(file_name) == ".rb" }
-changed_files = changed_files.join(" ")
-
-exit(0) if changed_files.empty?
-
-success = system(%(
-  rubocop #{changed_files}
-))
-
-STDIN.reopen('/dev/tty')
-
-if success == false
-  puts "Would you like to continue press 'any key' or 'n/N' to halt? "
-  exit(1) if %w(N n).include?(gets.chomp)
-end
+# Запуск RuboCop перед каждым коммитом
+bundle exec rubocop
+if [ $? -ne 0 ]; then
+  echo "RuboCop found issues. Commit aborted."
+  exit 1
+fi
