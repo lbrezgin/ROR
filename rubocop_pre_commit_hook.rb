4a42b2d3e5f9396e4b85bcd8f956a30c9bf3d259
lbrezgin <lewickbrez@gmail.com>
2023-08-24 14:11:05 +0300

    pre commit disable

--- rubocop_pre_commit_hook.rb
+++ rubocop_pre_commit_hook.rb
@@ -1,27 +1,27 @@
-#!/usr/bin/env ruby
+# #!/usr/bin/env ruby
 
-ADDED_OR_MODIFIED = /^\s*(A|AM|M)/.freeze
+# ADDED_OR_MODIFIED = /^\s*(A|AM|M)/.freeze
 
-changed_files = `git status --porcelain`.split(/\n/)
-unstaged_files = `git ls-files -m`.split(/\n/)
+# changed_files = `git status --porcelain`.split(/\n/)
+# unstaged_files = `git ls-files -m`.split(/\n/)
 
-changed_files = changed_files.select { |f| f =~ ADDED_OR_MODIFIED }
-changed_files = changed_files.map { |f| f.split(" ")[1] }
+# changed_files = changed_files.select { |f| f =~ ADDED_OR_MODIFIED }
+# changed_files = changed_files.map { |f| f.split(" ")[1] }
 
-changed_files -= (unstaged_files - changed_files)
+# changed_files -= (unstaged_files - changed_files)
 
-changed_files = changed_files.select { |file_name| File.extname(file_name) == ".rb" }
-changed_files = changed_files.join(" ")
+# changed_files = changed_files.select { |file_name| File.extname(file_name) == ".rb" }
+# changed_files = changed_files.join(" ")
 
-exit(0) if changed_files.empty?
+# exit(0) if changed_files.empty?
 
-success = system(%(
-  rubocop #{changed_files}
-))
+# success = system(%(
+#   rubocop #{changed_files}
+# ))
 
-STDIN.reopen('/dev/tty')
+# STDIN.reopen('/dev/tty')
 
-if success == false
-  puts "Would you like to continue press 'any key' or 'n/N' to halt? "
-  exit(1) if %w(N n).include?(gets.chomp)
-end
+# if success == false
+#   puts "Would you like to continue press 'any key' or 'n/N' to halt? "
+#   exit(1) if %w(N n).include?(gets.chomp)
+# end
