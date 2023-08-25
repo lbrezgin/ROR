529143e0dab70a7b42c7ac28ba75f4f4fc1be73a
lbrezgin <lewickbrez@gmail.com>
2023-08-25 12:31:39 +0300

    ad code from video

--- code_from_video/theme8/metaprogramming.rb
+++ code_from_video/theme8/metaprogramming.rb
@@ -10,15 +10,15 @@
 #   end
 # end 
  
-# module MyAttrAccessor 
-#   def my_attr_accessor(*names)
-#     names.each do |name|
-#       var_name = "@#{name}".to_sym
-#       define_method(name) { instance_variable_get(var_name) }
-#       define_method("#{name}=".to_sym) { |value| instance_variable_set(var_name, value)}
-#     end
-#   end
-# end
+module MyAttrAccessor 
+  def my_attr_accessor(*names)
+    names.each do |name|
+      var_name = "@#{name}".to_sym
+      define_method(name) { instance_variable_get(var_name) }
+      define_method("#{name}=".to_sym) { |value| instance_variable_set(var_name, value)}
+    end
+  end
+end
 
 # class Test 
 #   extend MyAttrAccessor
